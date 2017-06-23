6e1b7e75fb4b4fac617f73eb4cd5581763edbca4
Wuersching <Ezh@Ezhs-MacBook-Pro.local>
2017-06-23 18:00:59 +1000

    I'll fix the typing issues sometime shortly; the unit tests are pretty lazy, I just wanted to get something committed.

---
+++ test/mockdata.ts
@@ -0,0 +1,23 @@
+
+//I figured it might make sense to keep some fake item data objects
+//so that they can be used for testing as needed. Didn't want to paste them
+//in to every test I make and messy up the place.
+
+
+let mockdata1 = {
+  name: "McDouble",
+  calories: 600
+}
+
+let mockdata2 = {
+  name: "QuarterPounder",
+  calories: 700
+}
+
+let mockdata3 = {
+  name: "DoubleQuarterPounder",
+  calories: 1200
+}
+
+
+export {mockdata1, mockdata2, mockdata3};
